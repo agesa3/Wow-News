0b8d0e0b10fcbcacf2bf2b74d78c9324f7f4fcbf
agesa3 <collinsagesa2000@gmail.com>
2022-04-30 23:59:34 +0300

    setup retrofit and logging interceptor

---
+++ app/src/main/java/com/agesadev/wownews/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.agesadev.wownews.utils
+
+class Constants {
+    companion object{
+        const val API_KEY="a26792cb7ab2472cb7e40f4165608a15"
+        const val BASE_URL="https://newsapi.org/"
+    }
+}
