2c03135e650b7399b7fb0029946ed4311e496969
agesa3 <collinsagesa2000@gmail.com>
2022-05-01 22:39:38 +0300

    pagination on breaking news page working ok

--- app/src/main/java/com/agesadev/wownews/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/agesadev/wownews/ui/fragments/BreakingNewsFragment.kt
@@ -6,13 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.agesadev.wownews.MainActivity
 import com.agesadev.wownews.R
 import com.agesadev.wownews.adapters.NewsAdapter
 import com.agesadev.wownews.ui.viewmodel.NewsViewModel
+import com.agesadev.wownews.utils.Constants.Companion.QUERY_PAGE_SIZE
 import com.agesadev.wownews.utils.Resource
 import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
@@ -28,8 +31,8 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
         setUpRecyclerView()
 
         newsAdapter.setOnItemClickListener {
-            val bundle= Bundle().apply{
-               putSerializable("art",it)
+            val bundle = Bundle().apply {
+                putSerializable("art", it)
             }
             findNavController().navigate(
                 R.id.action_breakingNewsFragment2_to_articlesFragment,
@@ -43,7 +46,10 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
                 is Resource.Success -> {
                     hideProgressBar()
                     response.data?.let { newsResponse ->
-                        newsAdapter.differ.submitList(newsResponse.articles)
+                        newsAdapter.differ.submitList(newsResponse.articles.toList())
+                        val totalPages = newsResponse.totalResults / QUERY_PAGE_SIZE + 2
+                        isLastPage = viewModel.breakingNewsPage == totalPages
+
                     }
                 }
                 is Resource.Error -> {
@@ -63,17 +69,57 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
     private fun hideProgressBar() {
         paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
     }
 
     private fun showProgressBar() {
         paginationProgressBar.visibility = View.VISIBLE
+        isLoading = true
     }
+
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    var scrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible = totalItemCount >= QUERY_PAGE_SIZE
+            val shouldPaginate =
+                isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning && isTotalMoreThanVisible && isScrolling
+            if (shouldPaginate) {
+                viewModel.getBreakingNews("us")
+                isScrolling = false
+            } else {
+                rvBreakingNews.setPadding(0, 0, 0, 0)
+            }
+
+        }
+    }
+
 
     private fun setUpRecyclerView() {
         newsAdapter = NewsAdapter()
         rvBreakingNews.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@BreakingNewsFragment.scrollListener)
         }
 
     }
--- app/src/main/java/com/agesadev/wownews/utils/Constants.kt
+++ app/src/main/java/com/agesadev/wownews/utils/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY="a26792cb7ab2472cb7e40f4165608a15"
         const val BASE_URL="https://newsapi.org/"
         const val SEARCH_NEWS_DELAY=500L
+        const val QUERY_PAGE_SIZE=20
     }
 }
